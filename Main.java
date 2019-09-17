880736d7107039bbb4e9f7b2bb286bacaf713295
DiegoMazega <diego.mazega@live.com>
2019-09-17 00:20:05 -0300

    Adiçao de entrada de dados pelo usario pro evento.

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
-
+import java.util.Scanner;
 class Main{
 
         public static void main(String[] args){
-            
+            Scanner in = new Scanner(System.in);
             Evento silencio = new  Evento();
 
-            silencio.setId(14);
-            silencio.setNome("silencio");
-            silencio.setOcorrencia(16, 9, 2019);
+            silencio.setId(in.nextInt());
+            silencio.setNome(in.next());
+            silencio.setOcorrencia(in.nextInt(), in.nextInt(), in.nextInt());
             
             System.out.printf("%s%n", silencio);
-
+            in.close();
         }
 }
